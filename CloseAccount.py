fd778acfd1ea18be6d6243c7cf1b2018e9529f41
EduardoPacheco12 <70536883+EduardoPacheco12@users.noreply.github.com>
2020-11-26 06:01:55 -0300

    Add files via upload

--- CloseAccount.py
+++ CloseAccount.py
@@ -1,10 +1,34 @@
+import UserSettings
+import os
+import MainScreen
+import shutil
 
 #ENCERRAR A CONTA
 
 def finalizar_conta(nome_cliente):
 
-    print("<!>ENCERRAR A CONTA<!>")
-    print("Digite 'voltar' para voltar para as configurações do usuário\n")
+    while(True):
 
-    opcao_encerrar = input("Você deseja encerrar essa conta?")
+        print("<!>ENCERRAR A CONTA<!>\n")
+
+        opcao_encerrar = input("Você deseja encerrar essa conta?\n")
+
+        if opcao_encerrar.lower() == 'não':
+            UserSettings.configuracoes_sistema(nome_cliente)
+            return
+        elif opcao_encerrar.lower() == 'sim':
+            senha_cliente = open(f'{nome_cliente}/senha.txt', "r")
+            senha_atual = senha_cliente.read()
+            senha_cliente.close()
+
+            senha = input("Digite sua senha atual pra prosseguir(Depois de apagada, você não poderá recuperar a sua conta):\n")
+
+            if senha != senha_atual:
+                print("Senha incorreta")
+                continue
+            else:
+                shutil.rmtree(nome_cliente)
+                MainScreen.tela_entrada()
+                return
+
 
--- UserSettings.py
+++ UserSettings.py
@@ -14,15 +14,15 @@ def configuracoes_sistema(nome_cliente):
         print("[2] Encerrar conta")
         print("Digite 'voltar' para voltar pra tela de usuário\n")
 
-        opcao = input("O que deseja fazer?")
+        opcao = input("O que deseja fazer?\n")
 
         if opcao.lower() == 'voltar': #DIGITANDO ESSA OPÇÃO VOCÊ RETORNA PRA TELA DE USUÁRIO
             UserScreen.tela_usuario(nome_cliente)
             return
         elif opcao == '1': #DIGITANDO ESSA OPÇÃO VOCÊ IRÁ PRA PARTE DA ALTERAÇÃO DE INFORMAÇÕES PESSOAIS
-            ChangeInformation.alteracao_informacoes()
+            ChangeInformation.alteracao_informacoes(nome_cliente)
         elif opcao == '2': #DIGITANDO ESSA OPÇÃO VOCÊ IRÁ PRA PARTE DE ENCERRAR A CONTA
-            CloseAccount.finalizar_conta()
+            CloseAccount.finalizar_conta(nome_cliente)
         else:
             print("Opção inválida, tente novamente")
             continue
